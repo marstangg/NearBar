9c874ba602f8441f03fe2fa6abcd398de0cc5a29
Ubuntu <ubuntu@ip-172-31-47-99.ec2.internal>
2019-07-23 17:53:07 +0000

    added a places details to the nearby search results

--- OnePage/js/app.js
+++ OnePage/js/app.js
@@ -1,79 +1,170 @@
- var map, infowindow;
-
- function initMap() {
-   // Try HTML5 geolocation.
-   if (navigator.geolocation) {
-     navigator.geolocation.getCurrentPosition(function(position) {
-       var myLoc = {
-         lat: position.coords.latitude,
-         lng: position.coords.longitude
-       };
-       map = new google.maps.Map(document.getElementById('map'), {
-         center: myLoc,
-         zoom: 15
-       });
-       infowindow = new google.maps.InfoWindow();
-       var service = new google.maps.places.PlacesService(map);
-       service.nearbySearch({
-         location: myLoc,
-         radius: 15000,
-         type: ['bar']
-       }, callback);
-     });
-   } else {
-     // Browser doesn't support Geolocation
-     handleLocationError(false, infoWindow, map.getCenter());
-   }
- }
-
- function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-   infoWindow.setPosition(pos);
-   infoWindow.setContent(browserHasGeolocation ?
-     'Error: The Geolocation service failed.' :
-     'Error: Your browser doesn\'t support geolocation.');
-   infoWindow.open(map);
- }
+ let pos;
+ let map;
+ let bounds;
+ let infoWindow;
+ let currentInfoWindow;
+ let service;
+ let infoPane;
  
- function callback(results, status) {
-   if (status === google.maps.places.PlacesServiceStatus.OK) {
-     for (var i = 0; i < results.length; i++) {
-       createMarker(results[i]);
+ function initMap() {
+     // Initialize map
+     bounds = new google.maps.LatLngBounds();
+     infoWindow = new google.maps.InfoWindow;
+     currentInfoWindow = infoWindow;
+     infoPane = document.getElementById('panel');
+     // Try HTML5 geolocation
+     if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(position => {
+             pos = {
+                 lat: position.coords.latitude,
+                 lng: position.coords.longitude
+             };
+             map = new google.maps.Map(document.getElementById('map'), {
+                 center: pos,
+                 zoom: 15
+             });
+             bounds.extend(pos);
+             infoWindow.setPosition(pos);
+             infoWindow.setContent('You are Here!');
+             infoWindow.open(map);
+             map.setCenter(pos);
+             // Call Places Nearby Search on user's location
+             getNearbyPlaces(pos);
+         }, () => {
+             // Browser supports geolocation, but user has denied permission
+             handleLocationError(true, infoWindow);
+         });
      }
-   }
+     else {
+         // Browser doesn't support geolocation
+         handleLocationError(false, infoWindow);
+     }
  }
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
-
-
-
-//  function createMarkers(places) {
-//         var bounds = new google.maps.LatLngBounds();
-//         var placesList = document.getElementById('places');
-
-//          var marker = new google.maps.Marker({
-//             map: map,
-//             icon: image,
-//             title: place.name,
-//             position: place.geometry.location
-//           });
-
-//           var li = document.createElement('li');
-//           li.textContent = place.name;
-//           placesList.appendChild(li);
-
-//           bounds.extend(place.geometry.location);
-//         }
-//         map.fitBounds(bounds);
-//       }
-
+ // Handle a geolocation error
+ function handleLocationError(browserHasGeolocation, infoWindow) {
+     // Set default location to Sydney, Australia
+     pos = { lat: 1.35, lng: 103.891 };
+     map = new google.maps.Map(document.getElementById('map'), {
+         center: pos,
+         zoom: 15
+     });
+     // Display an InfoWindow at the map center
+     infoWindow.setPosition(pos);
+     infoWindow.setContent(browserHasGeolocation ?
+         'Geolocation permissions denied. Using default location.' :
+         'Error: Your browser doesn\'t support geolocation.');
+     infoWindow.open(map);
+     currentInfoWindow = infoWindow;
+// Call Places Nearby Search on the default location
+     getNearbyPlaces(pos);
+ }
+ // Perform a Places Nearby Search Request
+ function getNearbyPlaces(position) {
+     let request = {
+         location: position,
+         rankBy: google.maps.places.RankBy.DISTANCE,
+         keyword: 'bar'
+     };
+     service = new google.maps.places.PlacesService(map);
+     service.nearbySearch(request, nearbyCallback);
+ }
+ // Handle the results (up to 20) of the Nearby Search
+ function nearbyCallback(results, status) {
+     if (status == google.maps.places.PlacesServiceStatus.OK) {
+         createMarkers(results);
+     }
+ }
+ // Set markers at the location of each place result
+ function createMarkers(places) {
+     places.forEach(place => {
+         let marker = new google.maps.Marker({
+             position: place.geometry.location,
+             map: map,
+             title: place.name
+         });
+         
+         // Add click listener to each marker
+         google.maps.event.addListener(marker, 'click', () => {
+             let request = {
+                 placeId: place.place_id,
+                 fields: ['name', 'formatted_address', 'geometry', 'rating',
+                     'website', 'photos'
+                 ]
+             };
+             /* Only fetch the details of a place when the user clicks on a marker*/
+    
+             service.getDetails(request, (placeResult, status) => {
+                 showDetails(placeResult, marker, status)
+             });
+         });
+         // Adjust the map bounds to include the location of this marker
+         bounds.extend(place.geometry.location);
+     });
+    
+      /* show all the markers within the visible area. */
+     map.fitBounds(bounds);
+ }
+ // InfoWindow to display details above the marker
+ function showDetails(placeResult, marker, status) {
+     if (status == google.maps.places.PlacesServiceStatus.OK) {
+         let placeInfowindow = new google.maps.InfoWindow();
+         let rating = "None";
+         if (placeResult.rating) rating = placeResult.rating;
+         placeInfowindow.setContent('<div><strong>' + placeResult.name +
+             '</strong><br>' + 'Rating: ' + rating + '</div>');
+         placeInfowindow.open(marker.map, marker);
+         currentInfoWindow.close();
+         currentInfoWindow = placeInfowindow;
+         showPanel(placeResult);
+     }
+     else {
+         console.log('showDetails failed: ' + status);
+     }
+ }
+ /* TODO: Step 4D: Load place details in a sidebar */
+ // Displays place details in a sidebar
+ function showPanel(placeResult) {
+     // If infoPane is already open, close it
+     if (infoPane.classList.contains("open")) {
+         infoPane.classList.remove("open");
+     }
+     // Clear the previous details
+     while (infoPane.lastChild) {
+         infoPane.removeChild(infoPane.lastChild);
+     }
+     // Add the primary photo, if there is one
+     if (placeResult.photos) {
+         let firstPhoto = placeResult.photos[0];
+         let photo = document.createElement('img');
+         photo.classList.add('hero');
+         photo.src = firstPhoto.getUrl();
+         infoPane.appendChild(photo);
+     }
+     // Add place details with text formatting
+     let name = document.createElement('h1');
+     name.classList.add('place');
+     name.textContent = placeResult.name;
+     infoPane.appendChild(name);
+     if (placeResult.rating) {
+         let rating = document.createElement('p');
+         rating.classList.add('details');
+         rating.textContent = `Rating: ${placeResult.rating} ✮`;
+         infoPane.appendChild(rating);
+     }
+     let address = document.createElement('p');
+     address.classList.add('details');
+     address.textContent = placeResult.formatted_address;
+     infoPane.appendChild(address);
+     if (placeResult.website) {
+         let websitePara = document.createElement('p');
+         let websiteLink = document.createElement('a');
+         let websiteUrl = document.createTextNode(placeResult.website);
+         websiteLink.appendChild(websiteUrl);
+         websiteLink.title = placeResult.website;
+         websiteLink.href = placeResult.website;
+         websitePara.appendChild(websiteLink);
+         infoPane.appendChild(websitePara);
+     }
+     // Open the infoPane
+     infoPane.classList.add("open");
+ }
